9087263a43239b3a120a89b18b36678beef9ae61
RyeNguyen <minhiq149@gmail.com>
2022-11-24 13:44:58 +0700

    add home screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import LoginFooter from '../modules/Login/LoginFooter';
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>This is a home screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
