99e3a8b1ca8581491708d98aa7d01302d3fe004d
HungNguyen <hungng1810@gmail.com>
2022-11-28 09:19:10 +0700

    back pre commit

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Images } from '../../assets';
-import CustomButton from '../components/CustomButton';
+import LoginFooter from '../modules/Login/LoginFooter';
 
 const LoginScreen = () => {
   return (
-    <View style={{ height: 60, alignSelf: 'center', marginTop: 30 }}>
-      <CustomButton text={'Click here!'} isPrimary />
+    <View>
+      <LoginFooter />
     </View>
   );
 };
-
 export default LoginScreen;
-
 const styles = StyleSheet.create({});
