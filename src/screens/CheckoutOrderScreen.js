8dfa9d38acf4fa3ff0bbf52118710e9e379938c0
KienHaVan <hakien351998@gmail.com>
2022-12-06 10:13:12 +0700

    handleAddress

--- src/screens/CheckoutOrderScreen.js
+++ src/screens/CheckoutOrderScreen.js
@@ -19,9 +19,12 @@ import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 import { addUserToFirebaseWithID } from '../utils/authentication';
 import { resetCart } from '../features/cartSlice';
+import { useEffect } from 'react';
 
 const CheckoutOrderScreen = () => {
   const [showLoader, setShowLoader] = useState(false);
+  const [address, setAddress] = useState('');
+  console.log('🚀 ~ file: CheckoutOrderScreen.js:27 ~ CheckoutOrderScreen ~ address', address);
   const navigation = useNavigation();
   const route = useRoute();
   const carts = useSelector((state) => state.cart.carts);
@@ -30,6 +33,19 @@ const CheckoutOrderScreen = () => {
   const paymentMethod = route.params.paymentMethod;
   const dispatch = useDispatch();
   const id = auth()?.currentUser?.uid;
+  useEffect(() => {
+    const checkAddress = async () => {
+      const data = await firestore().collection('users').doc(id).get();
+      const address = data.data().address;
+      if (!address) {
+        navigation.navigate('EditProfile');
+      } else {
+        setAddress(address);
+      }
+    };
+    checkAddress();
+  }, []);
+
   const handlePlaceOrder = async () => {
     setShowLoader(true);
     const data = await firestore().collection('users').doc(id).get();
@@ -43,9 +59,6 @@ const CheckoutOrderScreen = () => {
         auth()?.currentUser?.uid
       );
     }
-    // carts.map((one) => {
-    //   one.status = '0';
-    // });
     await firestore()
       .collection('users')
       .doc(id)
